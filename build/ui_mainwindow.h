8cb66f863f162945d0e10cf2493da586a38d768d
lu7did <lu7did@gmail.com>
2019-08-25 10:50:08 -0300

    GUI

--- build/ui_mainwindow.h
+++ build/ui_mainwindow.h
@@ -20,6 +20,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QProgressBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSlider>
 #include <QtWidgets/QSpinBox>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
@@ -50,17 +51,22 @@ public:
     QPushButton *toggleTransmit;
     QLabel *label_4;
     QComboBox *comboSampRate;
-    QPushButton *toggleLSB_2;
-    QPushButton *toggleLSB_3;
-    QLCDNumber *lcdNumber;
+    QPushButton *toggleCW;
+    QPushButton *toggleCWR;
+    QLCDNumber *lcdNumberPanel;
     QProgressBar *progressBarMeter;
-    QPushButton *pushButton;
-    QPushButton *pushButton_2;
-    QPushButton *pushButton_3;
-    QPushButton *pushButton_4;
-    QPushButton *pushButton_5;
-    QPushButton *pushButton_6;
+    QPushButton *pushButtonAB;
+    QPushButton *pushButtonA2B;
+    QPushButton *pushButtonABS;
+    QPushButton *pushButtonSplit;
+    QPushButton *pushButtonRIT;
+    QPushButton *pushButtonLock;
     QLabel *label_5;
+    QSlider *verticalSlider;
+    QSlider *verticalSlider_2;
+    QSlider *verticalSlider_3;
+    QSlider *verticalSlider_4;
+    QSlider *verticalSlider_5;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -175,48 +181,51 @@ public:
         comboSampRate->setObjectName(QStringLiteral("comboSampRate"));
         comboSampRate->setGeometry(QRect(60, 60, 100, 20));
         comboSampRate->setFont(font2);
-        toggleLSB_2 = new QPushButton(widgetControls);
-        toggleLSB_2->setObjectName(QStringLiteral("toggleLSB_2"));
-        toggleLSB_2->setGeometry(QRect(450, 0, 30, 20));
-        toggleLSB_2->setFont(font2);
-        toggleLSB_2->setCheckable(true);
-        toggleLSB_3 = new QPushButton(widgetControls);
-        toggleLSB_3->setObjectName(QStringLiteral("toggleLSB_3"));
-        toggleLSB_3->setGeometry(QRect(480, 0, 30, 20));
-        toggleLSB_3->setFont(font2);
-        toggleLSB_3->setCheckable(true);
-        lcdNumber = new QLCDNumber(widgetControls);
-        lcdNumber->setObjectName(QStringLiteral("lcdNumber"));
-        lcdNumber->setGeometry(QRect(360, 30, 231, 71));
+        toggleCW = new QPushButton(widgetControls);
+        toggleCW->setObjectName(QStringLiteral("toggleCW"));
+        toggleCW->setGeometry(QRect(450, 0, 30, 20));
+        toggleCW->setFont(font2);
+        toggleCW->setCheckable(true);
+        toggleCWR = new QPushButton(widgetControls);
+        toggleCWR->setObjectName(QStringLiteral("toggleCWR"));
+        toggleCWR->setGeometry(QRect(480, 0, 30, 20));
+        toggleCWR->setFont(font2);
+        toggleCWR->setCheckable(true);
+        lcdNumberPanel = new QLCDNumber(widgetControls);
+        lcdNumberPanel->setObjectName(QStringLiteral("lcdNumberPanel"));
+        lcdNumberPanel->setGeometry(QRect(360, 30, 231, 71));
+        lcdNumberPanel->setSmallDecimalPoint(true);
+        lcdNumberPanel->setDigitCount(7);
         progressBarMeter = new QProgressBar(widgetControls);
         progressBarMeter->setObjectName(QStringLiteral("progressBarMeter"));
         progressBarMeter->setGeometry(QRect(360, 110, 231, 15));
         progressBarMeter->setFont(font2);
         progressBarMeter->setValue(24);
-        pushButton = new QPushButton(widgetControls);
-        pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(300, 130, 40, 20));
-        pushButton->setFont(font2);
-        pushButton_2 = new QPushButton(widgetControls);
-        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
-        pushButton_2->setGeometry(QRect(350, 130, 40, 20));
-        pushButton_2->setFont(font2);
-        pushButton_3 = new QPushButton(widgetControls);
-        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
-        pushButton_3->setGeometry(QRect(400, 130, 40, 20));
-        pushButton_3->setFont(font2);
-        pushButton_4 = new QPushButton(widgetControls);
-        pushButton_4->setObjectName(QStringLiteral("pushButton_4"));
-        pushButton_4->setGeometry(QRect(450, 130, 40, 20));
-        pushButton_4->setFont(font2);
-        pushButton_5 = new QPushButton(widgetControls);
-        pushButton_5->setObjectName(QStringLiteral("pushButton_5"));
-        pushButton_5->setGeometry(QRect(500, 130, 40, 20));
-        pushButton_5->setFont(font2);
-        pushButton_6 = new QPushButton(widgetControls);
-        pushButton_6->setObjectName(QStringLiteral("pushButton_6"));
-        pushButton_6->setGeometry(QRect(550, 130, 40, 20));
-        pushButton_6->setFont(font2);
+        progressBarMeter->setTextVisible(false);
+        pushButtonAB = new QPushButton(widgetControls);
+        pushButtonAB->setObjectName(QStringLiteral("pushButtonAB"));
+        pushButtonAB->setGeometry(QRect(300, 130, 40, 20));
+        pushButtonAB->setFont(font2);
+        pushButtonA2B = new QPushButton(widgetControls);
+        pushButtonA2B->setObjectName(QStringLiteral("pushButtonA2B"));
+        pushButtonA2B->setGeometry(QRect(350, 130, 40, 20));
+        pushButtonA2B->setFont(font2);
+        pushButtonABS = new QPushButton(widgetControls);
+        pushButtonABS->setObjectName(QStringLiteral("pushButtonABS"));
+        pushButtonABS->setGeometry(QRect(400, 130, 40, 20));
+        pushButtonABS->setFont(font2);
+        pushButtonSplit = new QPushButton(widgetControls);
+        pushButtonSplit->setObjectName(QStringLiteral("pushButtonSplit"));
+        pushButtonSplit->setGeometry(QRect(450, 130, 40, 20));
+        pushButtonSplit->setFont(font2);
+        pushButtonRIT = new QPushButton(widgetControls);
+        pushButtonRIT->setObjectName(QStringLiteral("pushButtonRIT"));
+        pushButtonRIT->setGeometry(QRect(500, 130, 40, 20));
+        pushButtonRIT->setFont(font2);
+        pushButtonLock = new QPushButton(widgetControls);
+        pushButtonLock->setObjectName(QStringLiteral("pushButtonLock"));
+        pushButtonLock->setGeometry(QRect(550, 130, 40, 20));
+        pushButtonLock->setFont(font2);
         label_5 = new QLabel(widgetControls);
         label_5->setObjectName(QStringLiteral("label_5"));
         label_5->setGeometry(QRect(310, 110, 48, 15));
@@ -225,6 +234,31 @@ public:
         font3.setBold(false);
         font3.setWeight(50);
         label_5->setFont(font3);
+        verticalSlider = new QSlider(widgetControls);
+        verticalSlider->setObjectName(QStringLiteral("verticalSlider"));
+        verticalSlider->setGeometry(QRect(165, 10, 20, 100));
+        verticalSlider->setValue(10);
+        verticalSlider->setOrientation(Qt::Vertical);
+        verticalSlider_2 = new QSlider(widgetControls);
+        verticalSlider_2->setObjectName(QStringLiteral("verticalSlider_2"));
+        verticalSlider_2->setGeometry(QRect(187, 9, 20, 100));
+        verticalSlider_2->setValue(99);
+        verticalSlider_2->setOrientation(Qt::Vertical);
+        verticalSlider_3 = new QSlider(widgetControls);
+        verticalSlider_3->setObjectName(QStringLiteral("verticalSlider_3"));
+        verticalSlider_3->setGeometry(QRect(208, 9, 20, 100));
+        verticalSlider_3->setValue(75);
+        verticalSlider_3->setOrientation(Qt::Vertical);
+        verticalSlider_4 = new QSlider(widgetControls);
+        verticalSlider_4->setObjectName(QStringLiteral("verticalSlider_4"));
+        verticalSlider_4->setGeometry(QRect(230, 10, 20, 100));
+        verticalSlider_4->setValue(25);
+        verticalSlider_4->setOrientation(Qt::Vertical);
+        verticalSlider_5 = new QSlider(widgetControls);
+        verticalSlider_5->setObjectName(QStringLiteral("verticalSlider_5"));
+        verticalSlider_5->setGeometry(QRect(252, 10, 20, 100));
+        verticalSlider_5->setValue(50);
+        verticalSlider_5->setOrientation(Qt::Vertical);
 
         verticalLayout->addWidget(widgetControls);
 
@@ -268,15 +302,15 @@ public:
          << QApplication::translate("MainWindow", "480000", Q_NULLPTR)
          << QApplication::translate("MainWindow", "240000", Q_NULLPTR)
         );
-        toggleLSB_2->setText(QApplication::translate("MainWindow", "CW", Q_NULLPTR));
-        toggleLSB_3->setText(QApplication::translate("MainWindow", "CWR", Q_NULLPTR));
+        toggleCW->setText(QApplication::translate("MainWindow", "CW", Q_NULLPTR));
+        toggleCWR->setText(QApplication::translate("MainWindow", "CWR", Q_NULLPTR));
         progressBarMeter->setFormat(QApplication::translate("MainWindow", "%p", Q_NULLPTR));
-        pushButton->setText(QApplication::translate("MainWindow", "A/B", Q_NULLPTR));
-        pushButton_2->setText(QApplication::translate("MainWindow", "A>B", Q_NULLPTR));
-        pushButton_3->setText(QApplication::translate("MainWindow", "A<>B", Q_NULLPTR));
-        pushButton_4->setText(QApplication::translate("MainWindow", "Split", Q_NULLPTR));
-        pushButton_5->setText(QApplication::translate("MainWindow", "RIT", Q_NULLPTR));
-        pushButton_6->setText(QApplication::translate("MainWindow", "Lock", Q_NULLPTR));
+        pushButtonAB->setText(QApplication::translate("MainWindow", "A/B", Q_NULLPTR));
+        pushButtonA2B->setText(QApplication::translate("MainWindow", "A>B", Q_NULLPTR));
+        pushButtonABS->setText(QApplication::translate("MainWindow", "A<>B", Q_NULLPTR));
+        pushButtonSplit->setText(QApplication::translate("MainWindow", "Split", Q_NULLPTR));
+        pushButtonRIT->setText(QApplication::translate("MainWindow", "RIT", Q_NULLPTR));
+        pushButtonLock->setText(QApplication::translate("MainWindow", "Lock", Q_NULLPTR));
         label_5->setText(QApplication::translate("MainWindow", "Meter", Q_NULLPTR));
     } // retranslateUi
 
